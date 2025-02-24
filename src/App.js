1bc52e170eb8b3a1522b32b4d0797715c69a7fd5
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-02-23 22:33:08 -0600

    Initial commit. Adding files to use to the repository.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}

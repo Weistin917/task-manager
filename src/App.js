5e49d8969c50ea6d076920a76e697f037c61ae61
Wei-Chung Cheng <weichung.cheng@galileo.edu>
2025-02-24 16:14:07 -0600

    6th commit. Commented the code.

--- src/App.js
+++ src/App.js
@@ -7,25 +7,29 @@ import Viewer from "./components/Viewer";
 
 export default function App() {
   const categories = ["Personal", "Work", "Studies", "Family"];
-  const [tasks, setTasks] = useState(new Map());
-  const [showCat, setShowCat] = useState("All");
+  const [tasks, setTasks] = useState(new Map()); // Map of tasks; maps the task name to the task category
+  const [showCat, setShowCat] = useState("All"); // String that represents the category to show
 
+  // adds a new task to the list
   function addTask(taskName, category) {
     newTasks = new Map(tasks);
     newTasks.set(taskName, category);
     setTasks(newTasks);
   }
 
+  // deletes a task from the list
   function deleteTask(taskName) {
     newTasks = new Map(tasks);
     newTasks.delete(taskName);
     setTasks(newTasks);
   }
 
+  // filters the category to show
   function filterCategory(category) {
     setShowCat(category);
   }
 
+  // gets a copy of the tasks containing only the tasks to show
   function getShowTasks() {
     retMap = new Map();
     for (const t of tasks.keys()) {
